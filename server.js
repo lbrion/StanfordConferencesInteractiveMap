652aa5eefacad0a2c33dcf291db12dc2e0570e32
lbrion <lbrion@stanford.edu>
2014-07-14 09:10:28 -0700

    Project Creation
    First commit, set up project

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+var AWS = require('aws-sdk');
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+//GET home page.
+app.get('/', function (req, res) {
+  res.render('index', { appTitle: 'Stanford Conferences Interactive Map' });
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
